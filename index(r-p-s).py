d1b0ae0b63e99735d590eb474b7b70a1802b4256
Supriya334 <124588951+Supriya334@users.noreply.github.com>
2023-10-18 10:50:54 +0530

    codesoft_rock_paper_scissors

--- index(r-p-s).py
+++ index(r-p-s).py
@@ -7,24 +7,24 @@ while running:
     player=None
     computer=random.choice(options)
     while player not in options:
-        player=input("enter a choice(  rock,    paper,  scissors  ): ")
+        player=input("enter a choice(  rock🥌,    paper🧻,  scissors✂️  ): ")
     print(f"Player:{player}")
     print(f"Computer:{computer}")
     if (player==computer):
-        print("Tie:ribbon:")
-    elif(player==   "rock"   and computer==   "scissors"   ):
+        print("Tie🎀")
+    elif(player==   "rock"   and computer==   "scissors"):
         my_score+=1
-        print("You Win! :sunglasses:")
-    elif(player==   "paper"    and computer ==  "rock"      ):
+        print("You Win!🧝🏻")
+    elif(player==   "paper"    and computer ==  "rock"):
         my_score+=1
-        print("You Win!:sunglasses: ")
+        print("You Win!🧝🏻 ")
     elif(player==   "scissors" and computer== "paper"    ):
         my_score+=1
-        print("You Win! :sunglasses:")
+        print("You Win! 🧝🏻")
     else:
         com_score+=1
-    print("Computer Win! :computer:","My_Score",my_score,"Computer _Score",com_score)
+    print("Computer Win!:computer:","My_Score",my_score,"Computer _Score",com_score)
     play_again= input("play again?(yes/no):").lower()
     if not play_again=="yes":
         running=False
-print("Thanks For Playing:heart:")
+print("Thanks For Playing❤️")
